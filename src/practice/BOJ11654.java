904b058e44877e4d1229b99df10d914dd5c884b0
YongJun Cho <as2686@naver.com>
2022-06-24 09:24:50 +0900

    BOJ11654

---
+++ src/practice/BOJ11654.java
@@ -0,0 +1,14 @@
+package practice;
+import java.util.*;
+
+public class BOJ11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		System.out.println((int)c);
+		
+		
+		
+	}
+	
+}
