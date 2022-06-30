67b5b30b5c5242a6aff5aa87fc7ee24502e71fc5
YongJun Cho <as2686@naver.com>
2022-06-30 21:43:16 +0900

    BOJ1152

---
+++ src/practice/BOJ1152.java
@@ -0,0 +1,21 @@
+package practice;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class BOJ1152 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		System.out.println(st.countTokens());
+		
+		
+		
+		
+	}
+
+}
