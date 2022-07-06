ad24f154dbeda7c8cdde1a5cba7315987a2013a4
YongJun Cho <as2686@naver.com>
2022-07-06 23:59:17 +0900

    BOJ1934

---
+++ src/practice/BOJ1934.java
@@ -0,0 +1,24 @@
+package practice;
+import java.util.*;
+
+public class BOJ1934 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T= sc.nextInt();
+		for(int i=0;i<T;i++) {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println((a*b)/gcd(a,b));
+		}
+		
+		
+	}
+	public static int gcd(int a,int b) {
+		if(b==0)
+			return a;
+		else
+			return gcd(b,a&b);
+	}
+
+}
