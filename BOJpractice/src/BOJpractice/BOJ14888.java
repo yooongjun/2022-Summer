5188f68b7890925ffc8f619e3fe7d12e8a295747
YongJun Cho <as2686@naver.com>
2022-07-15 23:59:12 +0900

    BOJ14888

---
+++ BOJpractice/src/BOJpractice/BOJ14888.java
@@ -0,0 +1,83 @@
+package BOJpractice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14888 {
+	static int n;
+	static int arr[]; // ¼ö¿­
+	static int ort[]; //operator 
+	static int max = Integer.MIN_VALUE;
+	static int min = Integer.MAX_VALUE;
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		ort = new int[4];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<arr.length;i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<ort.length;i++)
+			ort[i] = Integer.parseInt(st.nextToken());
+
+		func(arr[0],1);
+		System.out.println(max+"\n"+min);
+	}
+	
+	static void func(int num, int idx) {
+
+			if(idx == n) {
+				if(num>max) {
+					max = num;
+				}
+				if(num<min) {
+					min = num;
+				}
+				return;
+			}
+		
+			int result=0;
+			for(int i=0;i<ort.length;i++) {
+				if(ort[i]!=0) {
+					ort[i]--;
+				
+				switch(i){
+					case 0:
+						result = num+ arr[idx];
+						break;
+					case 1:
+						result = num- arr[idx];
+						break;
+					case 2:
+						result = num*arr[idx];
+						break;
+					case 3:
+						if(num<0&&arr[idx]>0)
+						{
+							num*=-1;
+							result = num /arr[idx];
+							result *=-1;
+						}else
+							result = num/arr[idx];
+						break;
+					}
+				
+				}
+				func(result,idx+1);
+				ort[i]++;
+		}
+		
+		
+	}
+	
+	
+	
+	
+
+}
