876c1da53716aba899f4fee6908a0fc259407d9a
YongJun Cho <as2686@naver.com>
2022-07-25 23:59:26 +0900

    BOJ4153

---
+++ BOJpractice/src/BOJpractice/BOJ4153.java
@@ -0,0 +1,24 @@
+package BOJpractice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ4153 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int arr[] = new int[3];
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			arr[0] = Integer.parseInt(st.nextToken());
+			arr[1] = Integer.parseInt(st.nextToken());
+			arr[2] = Integer.parseInt(st.nextToken());
+			
+		}		
+	}
+
+}
