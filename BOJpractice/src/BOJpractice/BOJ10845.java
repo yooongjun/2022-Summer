d51a48e7a456ddd4a1db56fe8fa10c1a62fdddac
YongJun Cho <as2686@naver.com>
2022-08-23 00:06:18 +0900

    BOJ10845

--- BOJpractice/src/BOJpractice/BOJ10845.java
+++ BOJpractice/src/BOJpractice/BOJ10845.java
@@ -1,22 +1,21 @@
 package BOJpractice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BOJ10845 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		ArrayList<Integer> q = new ArrayList<>();
 		StringTokenizer st; 
-		int n = sc.nextInt();
-		sc.nextLine();
+		int n = Integer.parseInt(br.readLine());
 		
 		for(int i=0;i<n;i++) {
-			String s = sc.nextLine();
-			st = new StringTokenizer(s);
+			st = new StringTokenizer(br.readLine());
 			switch(st.nextToken()) {
 			case "push":
 				q.add(Integer.parseInt(st.nextToken()));
@@ -38,7 +37,7 @@ public class BOJ10845 {
 				else
 					System.out.println(0);
 				break;
-			case "fornt":
+			case "front":
 				if(q.isEmpty())
 					System.out.println(-1);
 				else
