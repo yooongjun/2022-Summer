3f06ef5c30271ecbf32df50f68e20d2b8aa90c41
YongJun Cho <as2686@naver.com>
2022-08-28 00:01:15 +0900

    BOJ10757

---
+++ BOJpractice/src/BOJpractice/BOJ10757.java
@@ -0,0 +1,18 @@
+package BOJpractice;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ10757 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger n1 = new BigInteger(sc.next());
+		BigInteger n2 = new BigInteger(sc.next());
+		
+		System.out.println(n1.add(n2));
+		
+	}
+	
+
+}
