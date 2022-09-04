a5171bd02b6ab0e6bb9105e2edf83d6346399044
YongJun Cho <as2686@naver.com>
2022-09-04 23:27:01 +0900

    BOJ10817

---
+++ BOJpractice/src/BOJpractice/BOJ10817.java
@@ -0,0 +1,17 @@
+package BOJpractice;
+
+import java.util.Scanner;
+
+public class BOJ10817 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println(a+b+c - Math.max(Math.max(a, b),c) - Math.min(a, Math.min(b, c)));
+		
+		
+	}
+
+}
