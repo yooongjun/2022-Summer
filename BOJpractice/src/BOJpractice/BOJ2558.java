c27fe4bd5f77409af1a1a0f90fe365cba39c56d2
YongJun Cho <as2686@naver.com>
2022-09-09 23:58:44 +0900

    BOJ2558

---
+++ BOJpractice/src/BOJpractice/BOJ2558.java
@@ -0,0 +1,15 @@
+package BOJpractice;
+
+import java.util.Scanner;
+
+public class BOJ2558 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		
+	}
+
+}
