2f483e5beca62544c98ce207853373490a535d3b
YongJun Cho <as2686@naver.com>
2022-09-20 23:58:49 +0900

    BOJ1712

---
+++ BOJpractice/src/BOJpractice/BOJ1712.java
@@ -0,0 +1,23 @@
+package BOJpractice;
+
+import  java.util.*;
+
+public class BOJ1712 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A,B,C;
+		A=sc.nextInt();
+		B=sc.nextInt();
+		C=sc.nextInt();
+		if(C<=B)
+			System.out.println(-1);
+		else
+			System.out.println(A/(C-B)+1);
+		
+		
+		sc.close();
+		
+	}
+
+}
